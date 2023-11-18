b7b016a3917619cacd1d867d4ab86e227eeb3c0d
oppara <oppara@oppara.tv>
2023-11-19 08:37:00 +0900

    feat: add middleware for restrict by ipaddress

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse, NextRequest } from 'next/server';
+
+const validAddresses = new Set(
+  process.env.VALID_ADDRESSES.split(',').map((address) => address.trim())
+);
+
+export function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+
+  let ip: string = req.ip ?? req.headers.get('x-real-ip') ?? '';
+
+  // with CDN or LB
+  if (!ip && req.headers.get('x-forwarded-for')) {
+    ip = req.headers.get('x-forwarded-for')?.split(',')[0] ?? '';
+  }
+
+  const tmp = `request.ip = ${req.ip}, request.nextUrl.host = ${req.nextUrl.host}`;
+
+  if (!validAddresses.has(ip)) {
+    console.info(`401 ${tmp}`);
+    return new NextResponse(null, { status: 401 });
+  }
+
+  return res;
+};
+
